7d8c20e916210e9442557ab57ac058c4a8be3770
johnvelis <john@johnvelis.com>
2020-09-17 08:05:28 -0400

    Pre-Update and Tested: Service Interface

--- Demo_MVVMBasic/BusinessLayer/WidgetRepository.cs
+++ Demo_MVVMBasic/BusinessLayer/WidgetRepository.cs
@@ -3,8 +3,6 @@ using Demo_MVVMBasic.DataAccessLayer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Demo_MVVMBasic.BusinessLayer
 {
